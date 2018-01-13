e3de12116a65436ba98ff824f32cedd3e0dc4116
Ingrid Wong <yw340@georgetown.edu>
2018-01-13 20:22:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.detect {|ingredient| cheese_types}
+  ingredients.detect {|ingredient| in cheese_types}
 end
